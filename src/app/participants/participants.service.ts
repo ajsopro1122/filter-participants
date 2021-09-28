213e645efa15240a2302c9eccddac64e06182d3d
ajsopro1122 <71369722+ajsopro1122@users.noreply.github.com>
2021-09-28 23:03:38 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,13 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "Tera Gunner", age:23 + " years old", gender: "Female"},
+            {name: "Brawler Tera", age:18 + " years old", gender: "Male"},
+            {name: "Tera Mystic", age:29 + " years old", gender: "Female"},
+            {name: "Gwapo Alvin", age:27 + " years old", gender: "Female"},
+            {name: "Si Alvin Gwapo", age:21 + " years old", gender: "Male"},
+            {name: "Lancer Velika", age:17 + " years old", gender: "Male"},
+        ];
+    }
+
+}
